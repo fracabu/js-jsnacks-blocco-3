32ca0fa9e0a6af9a77298688e56666658fb6b0ff
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-26 14:29:46 +0200

    modifiche

--- snacks2/script.js
+++ snacks2/script.js
@@ -6,9 +6,24 @@ Ogni volta che ne crei uno, stampalo.
 
 //creo prompt dove utente inserisce un numero chiamato N
 
-
-
+const numeroUtente = parseInt(prompt("innerisci un numero"));
+const arrayGenerati = [];
 //Generare N array formati da 10 numeri casuali da 1 a 100
+
+while (arrayGenerati.length < numeroUtente){
+    const nuovoArray=[];
+    
+while (nuovoArray.lenght <10){
+    const numeroUtente = Math.floor(Math.random() * 100) +1;
+
+    if(!nuovoArray.includes (numeroCasuale)){
+        nuovoArray.push (numeroUtente);
+        
+    }
+}
+arrayGenerati.push(nuovoArray);
+
+}
 
 
 
